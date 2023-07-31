65058f008bb016ce92c02dd343d9b1d0d6443fe4
Aditya Trivedi <atadityatrivedi9@gmail.com>
2023-07-31 13:21:33 +0530

    navbar updtaed

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,139 +1,172 @@
 import React from "react";
 import {
-	AppBar,
-	Box,
-	Toolbar,
-	IconButton,
-	Typography,
-	Button,
-	Drawer,
-	List,
-	ListItem,
-	ListItemText,
-	Hidden,
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Hidden,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const pages = [
-	{
-		path: "/",
-		title: "Home",
-	},
-
-	{
-		path: "/products",
-		title: "Products",
-	},
-	{
-		path: "/ingredient",
-		title: "Ingredient",
-	},
-	{
-		path: "/shop",
-		title: "Shop",
-	},
-	{
-		path: "/about",
-		title: "About",
-	},
-	{
-		path: "/blog",
-		title: "Blog",
-	},
-	{
-		path: "/impact",
-		title: "Impact",
-	},
+  {
+    path: "/",
+    title: "Home",
+  },
+  {
+    path: "/products",
+    title: "Products",
+  },
+  {
+    path: "/ingredient",
+    title: "Ingredient",
+  },
+  {
+    path: "/shop",
+    title: "Shop",
+  },
+  {
+    path: "/about",
+    title: "About",
+  },
+  {
+    path: "/blog",
+    title: "Blog",
+  },
+  {
+    path: "/impact",
+    title: "Impact",
+  },
 ];
 
 const Header = () => {
-	const [drawerOpen, setDrawerOpen] = React.useState(false);
-	const [selectedPage, setSelectedPage] = React.useState(pages[0].path); // Initially select the first page
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+  const [selectedPage, setSelectedPage] = React.useState(pages[0].path); // Initially select the first page
 
-	const toggleDrawer = () => {
-		setDrawerOpen(!drawerOpen);
-	};
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
 
-	const closeDrawer = () => {
-		setDrawerOpen(false);
-	};
+  const closeDrawer = () => {
+    setDrawerOpen(false);
+  };
 
-	const handlePageClick = (path) => {
-		setSelectedPage(path);
-		closeDrawer();
-	};
+  const handlePageClick = (path) => {
+    setSelectedPage(path);
+    closeDrawer();
+  };
 
-	return (
-		<AppBar position="static" color="default" sx={{ backgroundColor: "#fff", color: "#000", fontWeight: "bold" }}>
-			<Toolbar>
-				
+  return (
+    <AppBar
+      position="static"
+      color="default"
+      sx={{ backgroundColor: "#fff", color: "#000", fontWeight: "bold" }}
+    >
+      <Toolbar>
+        <Hidden mdUp>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Hidden>
 
-				<Hidden mdUp>
-					<IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer}>
-						<MenuIcon />
-					</IconButton>
-				</Hidden>
+        <Hidden mdDown>
+          <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", width: "100%" }}>
+            {/* Left Section */}
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              {pages.map((page) => (
+                <Button
+                  key={page.title}
+                  color="inherit"
+                  component={NavLink}
+                  to={page.path}
+                  sx={{
+                    mx: 1,
+                    textDecoration: selectedPage === page.path ? "underline" : "none",
+                    fontWeight: selectedPage === page.path ? "bold" : "normal",
+                  }}
+                  onClick={() => handlePageClick(page.path)}
+                >
+                  <Typography
+                    variant="subtitle1"
+                    sx={{
+                      color: selectedPage === page.path ? "#FF7276" : "inherit",
+                      fontWeight: selectedPage === page.path ? "bold" : "normal",
+                    }}
+                  >
+                    {page.title}
+                  </Typography>
+                </Button>
+              ))}
+             
+            </Box>
 
-				<Hidden mdDown>
-					<Box sx={{ display: "flex" }}>
-						{pages.map((page) => (
-							<Button
-								key={page.title}
-								color="inherit"
-								component={NavLink}
-								to={page.path}
-								sx={{
-									mx: 1,
-									textDecoration: selectedPage === page.path ? "underline" : "none",
-									fontWeight: selectedPage === page.path ? "bold" : "normal",
-								}}
-								onClick={() => handlePageClick(page.path)}
-							>
-								<Typography
-									variant="subtitle1"
-									sx={{
-										color: selectedPage === page.path ? "#FF7276" : "inherit",
-										fontWeight: selectedPage === page.path ? "bold" : "normal",
-									}}
-								>
-									{page.title}
-								</Typography>
-								
-							</Button>
-						))}
-						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-					<img src={logo} alt="logo" style={{ width: "100px" }} />
-				</Typography>
-			{/* add login , shop now and cart */}
-					</Box>
-				</Hidden>
-			</Toolbar>
+            {/* Right Section */}
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+			<Typography variant="h6" component="div">
+                <img src={logo} alt="logo" style={{ width: "100px" }} />
+              </Typography>
+              <Button
+                color="inherit"
+                component={NavLink}
+                to="/login"
+                sx={{ mx: 1 }}
+              >
+                Login
+              </Button>
+              <Button
+                variant="contained"
+                color="secondary"
+                component={NavLink}
+                to="/shop-now"
+                sx={{ mx: 1 }}
+              >
+                Shop Now
+              </Button>
+              <IconButton color="inherit" component={NavLink} to="/cart">
+                {/* You can replace the shop icon below with your desired shop icon */}
+                <ShoppingCartOutlinedIcon />
+              </IconButton>
+            </Box>
+          </Box>
+        </Hidden>
+      </Toolbar>
 
-			<Hidden mdUp>
-				<Drawer anchor="left" open={drawerOpen} onClose={closeDrawer}>
-					<List>
-						{pages.map((page) => (
-							<ListItem
-								key={page.title}
-								onClick={() => handlePageClick(page.path)}
-								component={NavLink}
-								to={page.path}
-								sx={{
-									color: selectedPage === page.path ? "#FF7276" : "inherit",
-								}}
-							>
-								<ListItemText primary={page.title} />
-							</ListItem>
-						))}
-					</List>
-				</Drawer>
-			</Hidden>
-
-			
-		</AppBar>
-	);
+      <Hidden mdUp>
+        <Drawer anchor="left" open={drawerOpen} onClose={closeDrawer}>
+          <List>
+            {pages.map((page) => (
+              <ListItem
+                key={page.title}
+                onClick={() => handlePageClick(page.path)}
+                component={NavLink}
+                to={page.path}
+                sx={{
+                  color: selectedPage === page.path ? "#FF7276" : "inherit",
+                }}
+              >
+                <ListItemText primary={page.title} />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+      </Hidden>
+    </AppBar>
+  );
 };
 
 export default Header;
