bf1459b921bdf9c8b1bbb30861254fc9a02a038c
Aditya Trivedi <atadityatrivedi9@gmail.com>
2023-07-31 13:04:22 +0530

    navbar added with dynamic routing and pages added

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,9 +1,139 @@
-import React from 'react'
+import React from "react";
+import {
+	AppBar,
+	Box,
+	Toolbar,
+	IconButton,
+	Typography,
+	Button,
+	Drawer,
+	List,
+	ListItem,
+	ListItemText,
+	Hidden,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.png";
+
+const pages = [
+	{
+		path: "/",
+		title: "Home",
+	},
+
+	{
+		path: "/products",
+		title: "Products",
+	},
+	{
+		path: "/ingredient",
+		title: "Ingredient",
+	},
+	{
+		path: "/shop",
+		title: "Shop",
+	},
+	{
+		path: "/about",
+		title: "About",
+	},
+	{
+		path: "/blog",
+		title: "Blog",
+	},
+	{
+		path: "/impact",
+		title: "Impact",
+	},
+];
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+	const [drawerOpen, setDrawerOpen] = React.useState(false);
+	const [selectedPage, setSelectedPage] = React.useState(pages[0].path); // Initially select the first page
 
-export default Header
+	const toggleDrawer = () => {
+		setDrawerOpen(!drawerOpen);
+	};
+
+	const closeDrawer = () => {
+		setDrawerOpen(false);
+	};
+
+	const handlePageClick = (path) => {
+		setSelectedPage(path);
+		closeDrawer();
+	};
+
+	return (
+		<AppBar position="static" color="default" sx={{ backgroundColor: "#fff", color: "#000", fontWeight: "bold" }}>
+			<Toolbar>
+				
+
+				<Hidden mdUp>
+					<IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer}>
+						<MenuIcon />
+					</IconButton>
+				</Hidden>
+
+				<Hidden mdDown>
+					<Box sx={{ display: "flex" }}>
+						{pages.map((page) => (
+							<Button
+								key={page.title}
+								color="inherit"
+								component={NavLink}
+								to={page.path}
+								sx={{
+									mx: 1,
+									textDecoration: selectedPage === page.path ? "underline" : "none",
+									fontWeight: selectedPage === page.path ? "bold" : "normal",
+								}}
+								onClick={() => handlePageClick(page.path)}
+							>
+								<Typography
+									variant="subtitle1"
+									sx={{
+										color: selectedPage === page.path ? "#FF7276" : "inherit",
+										fontWeight: selectedPage === page.path ? "bold" : "normal",
+									}}
+								>
+									{page.title}
+								</Typography>
+								
+							</Button>
+						))}
+						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+					<img src={logo} alt="logo" style={{ width: "100px" }} />
+				</Typography>
+			{/* add login , shop now and cart */}
+					</Box>
+				</Hidden>
+			</Toolbar>
+
+			<Hidden mdUp>
+				<Drawer anchor="left" open={drawerOpen} onClose={closeDrawer}>
+					<List>
+						{pages.map((page) => (
+							<ListItem
+								key={page.title}
+								onClick={() => handlePageClick(page.path)}
+								component={NavLink}
+								to={page.path}
+								sx={{
+									color: selectedPage === page.path ? "#FF7276" : "inherit",
+								}}
+							>
+								<ListItemText primary={page.title} />
+							</ListItem>
+						))}
+					</List>
+				</Drawer>
+			</Hidden>
+
+			
+		</AppBar>
+	);
+};
+
+export default Header;
---
+++ src/Routing/Routing.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Home from '../pages/Home'
+import Products from '../pages/Products'
+import Ingredient from '../pages/Ingredient'
+import Shop from '../pages/Shop'
+import About from '../pages/About'
+import Blog from '../pages/Blog'
+import Impact from '../pages/Impact'
+import Header from '../Layout/Header'
+
+const Routing = () => {
+  return (
+    <div>
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/products" element={<Products/>}/>
+        <Route exact path="/ingredient" element={<Ingredient/>}/>
+        <Route exact path="/shop" element={<Shop/>}/>
+        <Route exact path="/about" element={<About/>}/>
+        <Route exact path="/blog" element={<Blog/>}/>
+        <Route exact path="/impact" element={<Impact/>}/>
+
+
+      </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default Routing;
